f728a973fe7b5030db52c9b0f89329fdc75efc8a
ASUS <ASUS@DESKTOP-R67BL0J>
2021-03-12 07:44:39 +0700

    Method_Post

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+Silakan login dengan Username dan Password anda
+<form action="view.php" method="POST">
+		<table>
+			<tr>
+				<td>Username</td>
+				<td><input type="type" name="Username"></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td><input type="Password" name="Password"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" name="Login"></td>
+			</tr>
+		</table>
+</form>
+</body>
+</html>
---
+++ view.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+Username :<?php echo $_POST["Username"]?></br>
+Password :<?php echo $_POST["Password"]?>
+</body>
+</html>
